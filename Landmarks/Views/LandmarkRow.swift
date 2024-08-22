8b46ba8b48acd03597b9f9b8210d6cb1e41e19cd
Heather Haylett <heatherhaylett@gmail.com>
2024-08-22 14:12:36 -0600

    add identifiable

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Heather Haylett on 8/22/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
