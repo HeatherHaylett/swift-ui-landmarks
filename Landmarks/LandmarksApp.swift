f4740e9aab864a7da0b0fee7d51db92b53ac95b7
Heather Haylett <heatherhaylett@gmail.com>
2024-08-22 09:09:45 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Heather Haylett on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
